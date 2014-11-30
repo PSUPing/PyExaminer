ed733fa0c26f7a0903e87a70119a5679c9075ef9
Matt Ping <matthew.ping@psualum.com>
2014-11-30 11:21:26 -0500

    Changes for KNN run

--- cb.py
+++ cb.py
@@ -1,188 +1,246 @@
 from couchbase import Couchbase
-import numpy as np
 import joblib
 
-cb = Couchbase.connect(bucket='ex_ml',  host='localhost')
+#import numpy as np
 
-rows = cb.query("get", "fit_cpu_mem")
+bucket_name = 'examiner_data'
+host_name = '192.168.100.17'
 
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-y = [15]
+def get_run_data(design_doc, view_name, x_npy_file, y_npy_file, trn_run=False):
+    cb = Couchbase.connect(bucket=bucket_name,  host=host_name)
 
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
+    rows = cb.query(design_doc, view_name)
 
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap']])
-	if (row.value[0] == "clean_cpu_mem"):
-		y.append(0)
-	else: 
-		y.append(1)
-	count = count + 1
+    x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+    y = [15]
 
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-y.remove(15)
+    count = 0
+    for row in rows:
+        x.append([row.value[1]['thread_alloc_count'],
+                  row.value[1]['proc_count'],
+                  row.value[1]['thread_alloc_size'],
 
-joblib.dump(x, "fit_cpu_mem_x.npy")
-joblib.dump(y, "fit_cpu_mem_y.npy")
+                  row.value[2]['mem_free'],
+                  row.value[2]['native_allocated_heap'],
+                  row.value[2]['native_free_heap'],
+                  row.value[2]['mem_total'],
+                  row.value[2]['native_heap'],
 
-print 'Fitting Run - CPU / MEM: ' + `count`
+                  row.value[3]['global_class_init'],
+                  row.value[3]['classes_loaded'],
+                  row.value[3]['total_methods_invoc'],
 
-rows = cb.query("get", "fit_cpu_mem_dvm")
+                  row.value[4]['total_tx'],
+                  row.value[4]['total_rx']])
 
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-y = [15]
+        if trn_run == True:
+            y.append(0)
 
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
+        count = count + 1
 
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap'],
+    x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+    y.remove(15)
 
-			  row.value[3]['global_class_init'], 
-			  row.value[3]['classes_loaded'],
-			  row.value[3]['total_methods_invoc']])
-	if (row.value[0] == "clean_cpu_mem_dvm"):
-		y.append(0)
-	else: 
-		y.append(1)
-	count = count + 1
+    joblib.dump(x, x_npy_file)
+    joblib.dump(y, y_npy_file)
 
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-y.remove(15)
+    return count
 
-joblib.dump(x, "fit_cpu_mem_dvm_x.npy")
-joblib.dump(y, "fit_cpu_mem_dvm_y.npy")
 
-print 'Fitting Run - CPU / MEM / DVM: ' + `count`
+def get_mult_runs_data(design_doc, view_names, x_npy_file, y_npy_file):
+    cb = Couchbase.connect(bucket=bucket_name,  host=host_name)
 
-rows = cb.query("get", "fit_all")
+    x = [[0, 0, 0, 0, 0, 0, 0, 0]]
 
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-y = [15]
+    for view_name in view_names:
+        rows = cb.query(design_doc, view_name)
 
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
+        count = 0
+        for row in rows:
+            x.append([row.value[1]['thread_alloc_count'],
+                      row.value[1]['proc_count'],
+                      row.value[1]['thread_alloc_size'],
 
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap'],
+                      row.value[2]['mem_free'],
+                      row.value[2]['native_allocated_heap'],
+                      row.value[2]['native_free_heap'],
+                      row.value[2]['mem_total'],
+                      row.value[2]['native_heap'],
 
-			  row.value[3]['global_class_init'], 
-			  row.value[3]['classes_loaded'],
-			  row.value[3]['total_methods_invoc'],
+                      row.value[3]['global_class_init'],
+                      row.value[3]['classes_loaded'],
+                      row.value[3]['total_methods_invoc'],
 
-			  row.value[4]['total_tx'], 
-			  row.value[4]['total_rx']])
-	if (row.value[0] == "clean_all"):
-		y.append(0)
-	else: 
-		y.append(1)
-	count = count + 1
+                      row.value[4]['total_tx'],
+                      row.value[4]['total_rx']])
 
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-y.remove(15)
+            count = count + 1
 
-joblib.dump(x, "fit_all_x.npy")
-joblib.dump(y, "fit_all_y.npy")
+            print view_name + ' count: ' + `count`
 
-print 'Fitting Run - All: ' + `count`
+    x.remove([0, 0, 0, 0, 0, 0, 0, 0])
 
-###### Virus 2 ######################################
+    joblib.dump(x, x_npy_file)
 
-rows = cb.query("get", "virus2_cpu_mem")
+# "no_virus_trn_x.npy"
+# "no_virus_trn_y.npy"
 
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
-
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap']])
-	count = count + 1
-
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-
-joblib.dump(x, "virus2_cpu_mem_x.npy")
-
-print 'Virus 2 - CPU / MEM: ' + `count`
-
-rows = cb.query("get", "virus2_cpu_mem_dvm")
-
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
-
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap'],
-
-			  row.value[3]['global_class_init'], 
-			  row.value[3]['classes_loaded'],
-			  row.value[3]['total_methods_invoc']])
-	count = count + 1
-
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-
-joblib.dump(x, "virus2_cpu_mem_dvm_x.npy")
-
-print 'Virus 2 - CPU / MEM / DVM: ' + `count`
-
-rows = cb.query("get", "virus2_all")
-
-x = [[0, 0, 0, 0, 0, 0, 0, 0]]
-
-count = 0
-for row in rows:
-	x.append([row.value[1]['thread_alloc_count'], 
-			  row.value[1]['proc_count'],
-			  row.value[1]['thread_alloc_size'],
-
-			  row.value[2]['mem_free'],
-			  row.value[2]['native_allocated_heap'],
-			  row.value[2]['native_free_heap'],
-			  row.value[2]['mem_total'],
-			  row.value[2]['native_heap'],
-
-			  row.value[3]['global_class_init'], 
-			  row.value[3]['classes_loaded'],
-			  row.value[3]['total_methods_invoc'],
-
-			  row.value[4]['total_tx'], 
-			  row.value[4]['total_rx']])
-	count = count + 1
-
-x.remove([0, 0, 0, 0, 0, 0, 0, 0])
-
-joblib.dump(x, "virus2_all_x.npy")
-
-print 'Virus 2 - All: ' + `count`
+# cb = Couchbase.connect(bucket='examiner_data',  host='192.168.100.17')
+#
+# ###### No Virus Training Run ##########################
+#
+# rows = cb.query("get", "no_virus_trn")
+#
+# x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+# y = [15]
+#
+# count = 0
+# for row in rows:
+# 	x.append([row.value[1]['thread_alloc_count'],
+# 			  row.value[1]['proc_count'],
+# 			  row.value[1]['thread_alloc_size'],
+#
+# 			  row.value[2]['mem_free'],
+# 			  row.value[2]['native_allocated_heap'],
+# 			  row.value[2]['native_free_heap'],
+# 			  row.value[2]['mem_total'],
+# 			  row.value[2]['native_heap'],
+#
+# 			  row.value[3]['global_class_init'],
+# 			  row.value[3]['classes_loaded'],
+# 			  row.value[3]['total_methods_invoc'],
+#
+# 			  row.value[4]['total_tx'],
+# 			  row.value[4]['total_rx']])
+#     y.append(0)
+#
+# 	count = count + 1
+#
+# x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+# y.remove(15)
+#
+# joblib.dump(x, "no_virus_trn_x.npy")
+# joblib.dump(y, "no_virus_trn_y.npy")
+#
+# print 'Fitting Run - No Virus: ' + `count`
+#
+# ###### Anserver Bot ##########################
+#
+# rows = cb.query("get", "anserver")
+#
+# x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+# y = [15]
+#
+# count = 0
+# for row in rows:
+# 	x.append([row.value[1]['thread_alloc_count'],
+# 			  row.value[1]['proc_count'],
+# 			  row.value[1]['thread_alloc_size'],
+#
+# 			  row.value[2]['mem_free'],
+# 			  row.value[2]['native_allocated_heap'],
+# 			  row.value[2]['native_free_heap'],
+# 			  row.value[2]['mem_total'],
+# 			  row.value[2]['native_heap'],
+#
+# 			  row.value[3]['global_class_init'],
+# 			  row.value[3]['classes_loaded'],
+# 			  row.value[3]['total_methods_invoc'],
+#
+# 			  row.value[4]['total_tx'],
+# 			  row.value[4]['total_rx']])
+#     y.append(0)
+#
+# 	count = count + 1
+#
+# x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+# y.remove(15)
+#
+# joblib.dump(x, "no_virus_trn_x.npy")
+# joblib.dump(y, "no_virus_trn_y.npy")
+#
+# print 'Fitting Run - No Virus: ' + `count`
+#
+#
+#
+# ###### Virus 2 ######################################
+#
+# rows = cb.query("get", "virus2_cpu_mem")
+#
+# x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+#
+# count = 0
+# for row in rows:
+# 	x.append([row.value[1]['thread_alloc_count'],
+# 			  row.value[1]['proc_count'],
+# 			  row.value[1]['thread_alloc_size'],
+#
+# 			  row.value[2]['mem_free'],
+# 			  row.value[2]['native_allocated_heap'],
+# 			  row.value[2]['native_free_heap'],
+# 			  row.value[2]['mem_total'],
+# 			  row.value[2]['native_heap']])
+# 	count = count + 1
+#
+# x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+#
+# joblib.dump(x, "virus2_cpu_mem_x.npy")
+#
+# print 'Virus 2 - CPU / MEM: ' + `count`
+#
+# rows = cb.query("get", "virus2_cpu_mem_dvm")
+#
+# x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+#
+# count = 0
+# for row in rows:
+# 	x.append([row.value[1]['thread_alloc_count'],
+# 			  row.value[1]['proc_count'],
+# 			  row.value[1]['thread_alloc_size'],
+#
+# 			  row.value[2]['mem_free'],
+# 			  row.value[2]['native_allocated_heap'],
+# 			  row.value[2]['native_free_heap'],
+# 			  row.value[2]['mem_total'],
+# 			  row.value[2]['native_heap'],
+#
+# 			  row.value[3]['global_class_init'],
+# 			  row.value[3]['classes_loaded'],
+# 			  row.value[3]['total_methods_invoc']])
+# 	count = count + 1
+#
+# x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+#
+# joblib.dump(x, "virus2_cpu_mem_dvm_x.npy")
+#
+# print 'Virus 2 - CPU / MEM / DVM: ' + `count`
+#
+# rows = cb.query("get", "virus2_all")
+#
+# x = [[0, 0, 0, 0, 0, 0, 0, 0]]
+#
+# count = 0
+# for row in rows:
+# 	x.append([row.value[1]['thread_alloc_count'],
+# 			  row.value[1]['proc_count'],
+# 			  row.value[1]['thread_alloc_size'],
+#
+# 			  row.value[2]['mem_free'],
+# 			  row.value[2]['native_allocated_heap'],
+# 			  row.value[2]['native_free_heap'],
+# 			  row.value[2]['mem_total'],
+# 			  row.value[2]['native_heap'],
+#
+# 			  row.value[3]['global_class_init'],
+# 			  row.value[3]['classes_loaded'],
+# 			  row.value[3]['total_methods_invoc'],
+#
+# 			  row.value[4]['total_tx'],
+# 			  row.value[4]['total_rx']])
+# 	count = count + 1
+#
+# x.remove([0, 0, 0, 0, 0, 0, 0, 0])
+#
+# joblib.dump(x, "virus2_all_x.npy")
+#
+# print 'Virus 2 - All: ' + `count`
